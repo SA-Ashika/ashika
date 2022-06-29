43248d19e271d4366a32f3e5e0d78d205ebaa3fe
SA-Ashika <108018184+SA-Ashika@users.noreply.github.com>
2022-06-29 14:46:18 +0530

    Add files via upload

---
+++ 2 add.py
@@ -0,0 +1,6 @@
+a=input("Enter a number: ")
+b=input("Enter a  number: ")
+a=int(a)+int(b)
+b=int(a)-int(b)
+a=int(a)-int(b)
+print("a=(0) \nb=(1)", format(a,b))
---
+++ 4 kilometer.py
@@ -0,0 +1,4 @@
+kilometer=float(input("Enter the kilometer : "))
+cf=0.621371
+miles=float(kilometer)*float(cf)
+print ("{0} kilometers converted to {1} miles ". format(kilometer,miles))
